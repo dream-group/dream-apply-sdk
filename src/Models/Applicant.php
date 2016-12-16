d7160791cdfb34d00969e973e6c4e778995bb662
Anton Smirnov <anton.smirnov@dreamapply.com>
2016-12-16 21:38:17 +0200

    handle documents and photos

--- src/Models/Applicant.php
+++ src/Models/Applicant.php
@@ -21,24 +21,24 @@ namespace Dream\DreamApply\Client\Models;
  * @property-read string $phone
  * @property-read string $citizenship
  *
- * @property-read Record $photo
+ * @property-read Photo  $photo
  * @method        bool   photoExists()
  *
  * @method CollectionOfAddable|ApplicantTrackerAssociation[] trackers(array $filter = [])
- * @method Collection documents(array $filter = [])
- * @method Collection studyplans(array $filter = [])
+ * @method Collection|Document[] documents(array $filter = [])
+ * @method Collection|StudyPlan[] studyplans(array $filter = [])
  */
 class Applicant extends Record
 {
     const COLLECTION_CLASS = ApplicantCollection::class;
 
     protected $objectLinks = [
-        'photo'         => Record::class, // TODO: real class
+        'photo'         => Photo::class,
     ];
 
     protected $collectionLinks = [
         'trackers'      => ApplicantTrackerAssociation::class,
-        'documents'     => Record::class, // TODO: real class
+        'documents'     => Document::class,
         'studyplans'    => StudyPlan::class,
     ];
 }
---
+++ src/Models/Document.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sandfox
+ * Date: 16.12.16
+ * Time: 21:31
+ */
+
+namespace Dream\DreamApply\Client\Models;
+
+class Document extends BinaryRecord {}
