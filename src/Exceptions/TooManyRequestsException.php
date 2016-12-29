9d671efc5a20c3d3cace51cae15c76017bab8490
Anton Smirnov <anton.smirnov@dreamapply.com>
2016-12-29 18:03:29 +0200

    add exception for rate exceeded

---
+++ src/Exceptions/TooManyRequestsException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sandfox
+ * Date: 29/12/16
+ * Time: 18:02
+ */
+
+namespace Dream\DreamApply\Client\Exceptions;
+
+class TooManyRequestsException extends RuntimeException {}
--- src/Helpers/ExceptionHelper.php
+++ src/Helpers/ExceptionHelper.php
@@ -9,19 +9,31 @@
 namespace Dream\DreamApply\Client\Helpers;
 
 use Dream\DreamApply\Client\Exceptions\HttpFailResponseException;
+use Dream\DreamApply\Client\Exceptions\TooManyRequestsException;
 use Psr\Http\Message\ResponseInterface;
 
 class ExceptionHelper
 {
+    /**
+     * Handle some special cases and return general HTTP exception in other cases
+     *
+     * @param ResponseInterface $httpResponse
+     * @return \Exception|\Dream\DreamApply\Client\Exceptions\BaseException
+     */
     public static function fromResponse(ResponseInterface $httpResponse)
     {
-        $exception = new HttpFailResponseException(
-            implode(' ', ['Unsuccessful HTTP query:', $httpResponse->getStatusCode(), $httpResponse->getReasonPhrase()]),
-            $httpResponse->getStatusCode(),
-            null,
-            $httpResponse
-        );
+        // 2xx, 404 and some special cases should be handled earlier in the code
+        switch ($httpResponse->getStatusCode()) {
+            case 429:
+                return new TooManyRequestsException('Request rate exceeded');
 
-        return $exception;
+            default:
+                return new HttpFailResponseException(
+                    implode(' ', ['Unsuccessful HTTP query:', $httpResponse->getStatusCode(), $httpResponse->getReasonPhrase()]),
+                    $httpResponse->getStatusCode(),
+                    null,
+                    $httpResponse
+                );
+        }
     }
 }
