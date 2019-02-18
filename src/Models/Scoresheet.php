387956294d794ebf89991c18ce321463d9f7620a
Anton Smirnov <anton.smirnov@dreamapply.com>
2019-02-18 17:28:58 +0200

    Fix param name for scoresheet

--- src/Models/Scoresheet.php
+++ src/Models/Scoresheet.php
@@ -21,19 +21,19 @@ use Dream\DreamApply\Client\Exceptions\InvalidArgumentException;
 class Scoresheet extends Record
 {
     /**
-     * @param   int|AcademicTerm $academicTermID
+     * @param   int|AcademicTerm $byAcademicTermID
      * @return  array
      */
-    public function scores($academicTermID)
+    public function scores($byAcademicTermID)
     {
-        if (is_object($academicTermID)) {
-            if ($academicTermID instanceof AcademicTerm) {
-                $academicTermID = $academicTermID->id();
+        if (is_object($byAcademicTermID)) {
+            if ($byAcademicTermID instanceof AcademicTerm) {
+                $byAcademicTermID = $byAcademicTermID->id();
             } else {
                 throw new InvalidArgumentException('$academicTermID should be an integer or an instance of AcademicTerm');
             }
         }
 
-        return $this->client->http()->getJson($this->url . '/scores', ['academicTermID' => intval($academicTermID)]);
+        return $this->client->http()->getJson($this->url . '/scores', ['byAcademicTermID' => intval($byAcademicTermID)]);
     }
 }
