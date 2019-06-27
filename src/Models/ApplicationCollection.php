5df01ef4a5a429065392c570216657e8b810cb24
Anton Smirnov <anton.smirnov@dreamapply.com>
2019-06-27 10:43:32 +0300

    Initial support for application creation

--- src/Models/ApplicationCollection.php
+++ src/Models/ApplicationCollection.php
@@ -2,6 +2,8 @@
 
 namespace Dream\DreamApply\Client\Models;
 
+use Dream\DreamApply\Client\Exceptions\BaseException;
+
 /**
  * Class ApplicationCollection
  * @package Dream\DreamApply\Client\Models
@@ -12,9 +14,21 @@ namespace Dream\DreamApply\Client\Models;
  */
 class ApplicationCollection extends Collection
 {
+    use CollectionPlugins\CollectionOfCreatable;
+
     protected $collectionLinks = [
         'flags'     => Flag::class,
         'statuses'  => SimpleArray::class,
         'offers'    => OffersNamespace::class,
     ];
+
+    /**
+     * @param $postData
+     * @return Record
+     * @throws BaseException
+     */
+    public function create($postData)
+    {
+        return $this->doCreate($postData);
+    }
 }
