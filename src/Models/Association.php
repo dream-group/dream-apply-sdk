bbfd5b4b230f375f6a95c7bf5e2d6db3b7feedf2
Anton Smirnov <anton.smirnov@dreamapply.com>
2021-11-01 17:55:21 +0200

    Param names should be compatible too

--- src/Models/Association.php
+++ src/Models/Association.php
@@ -6,7 +6,7 @@ class Association extends Record
 {
     const ASSOCIATED_CLASS = null; // only required for addable associations
 
-    public function __construct($client, $url, array $data = [], $_partial = false)
+    public function __construct($client, $url, array $data = [], $partial = false)
     {
         parent::__construct($client, $url, $data, false); // association is never partial unless data[] is empty
     }
