bd1d2da65c583bd1335b80cc0c8fceb02582c051
Anton Smirnov <anton.smirnov@dreamapply.com>
2022-07-26 20:47:12 +0300

    Replace old Consent with generated

---
+++ gen/Models/Consent.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dream\Apply\Client\Models;
+
+use Dream\Apply\Client\Exceptions\InvalidArgumentException;
+use ArrayAccess;
+
+/**
+ * @generated
+ * @property-read string $type
+ * @property-read string $text
+ * @property-read string $link
+ */
+final class Consent implements ArrayAccess
+{
+    use BaseMethods\Record;
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->getData('type');
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->getData('text');
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->getData('link');
+    }
+
+    public function __get($name)
+    {
+        if ($name === 'type') {
+            return $this->getData('type');
+        }
+        if ($name === 'text') {
+            return $this->getData('text');
+        }
+        if ($name === 'link') {
+            return $this->getData('link');
+        }
+        throw new InvalidArgumentException(sprintf('Field "%s" does not exist in class "%s"', $name, self::class));
+    }
+
+    public function __isset($name)
+    {
+        return \in_array($name, ['type', 'text', 'link']) && $this->$name !== null;
+    }
+}
