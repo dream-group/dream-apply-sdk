b3bc19ba2f8c1814d744fdc7857b0ec7a56d31f6
Anton Smirnov <anton.smirnov@dreamapply.com>
2022-07-26 17:43:29 +0300

    Autoload for the old namespace

---
+++ bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dream\Apply\Client;
+
+const OLD_PREFIX = 'Dream\DreamApply\Client\\';
+const OLD_PREFIX_LEN = 24; // strlen(OLD_PREFIX_LEN)
+const NEW_PREFIX = 'Dream\Apply\Client\\';
+
+var_dump(strlen(OLD_PREFIX));
+
+spl_autoload_register(function ($class) {
+    if (strncmp($class, OLD_PREFIX, OLD_PREFIX_LEN) !== 0) {
+        return false;
+    }
+
+    $newClassName = NEW_PREFIX . substr($class, OLD_PREFIX_LEN);
+
+    if (!class_exists($newClassName)) {
+        return false;
+    }
+
+    class_alias($newClassName, $class);
+
+    return true;
+});
