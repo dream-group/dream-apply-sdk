e8ba3be20e56a2efb412760b176137730d6531e1
Anton Smirnov <anton.smirnov@dreamapply.com>
2022-07-26 20:29:57 +0300

    Replace old AcademicYear with generated

---
+++ gen/Models/AcademicYear.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Dream\Apply\Client\Models;
+
+use Dream\Apply\Client\Exceptions\InvalidArgumentException;
+use ArrayAccess;
+
+/**
+ * @generated
+ * @property-read string $name
+ * @property-read string $start
+ */
+final class AcademicYear implements ArrayAccess
+{
+    use BaseMethods\Record;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->getData('name');
+    }
+
+    /**
+     * @return string
+     */
+    public function getStart()
+    {
+        return $this->getData('start');
+    }
+
+    public function __get($name)
+    {
+        if ($name === 'name') {
+            return $this->getData('name');
+        }
+        if ($name === 'start') {
+            return $this->getData('start');
+        }
+        throw new InvalidArgumentException(sprintf('Field "%s" does not exist in class "%s"', $name, self::class));
+    }
+
+    public function __isset($name)
+    {
+        return \in_array($name, ['name', 'start']) && $this->$name !== null;
+    }
+}
