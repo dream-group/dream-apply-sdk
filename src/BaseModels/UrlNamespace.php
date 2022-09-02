fa1852648c4279ce045271a9428d5690304ba997
Anton Smirnov <anton.smirnov@dreamapply.com>
2022-09-02 21:26:46 +0300

    Allow namespaces to have child objects

--- src/BaseModels/UrlNamespace.php
+++ src/BaseModels/UrlNamespace.php
@@ -5,11 +5,22 @@ namespace Dream\Apply\Client\BaseModels;
 use Dream\Apply\Client\Client;
 use Dream\Apply\Client\Exceptions\BadMethodCallException;
 use Dream\Apply\Client\Exceptions\InvalidArgumentException;
+use Dream\Apply\Client\Helpers\ResponseHelper;
 use Dream\Apply\Client\Helpers\StringHelper;
 
 abstract class UrlNamespace
 {
     use ChildNamespaces;
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    abstract protected function getField($name);
+    /**
+     * @return array
+     */
+    abstract protected function getFieldList();
 
     /** @var Client */
     protected $client;
@@ -26,10 +37,33 @@ abstract class UrlNamespace
         $this->baseUrl  = $baseUrl;
     }
 
+    protected function hasObjectField($field)
+    {
+        $url = $this->baseUrl . '/' . $field;
+
+        $response = $this->client->http()->head($url);
+        return ResponseHelper::resourceExistsByResponse($response);
+    }
+
+    protected function getObjectField($field, $class)
+    {
+        $url = $this->baseUrl . '/' . $field;
+
+        return new $class($this->client, $url, [], true);
+    }
+
+    protected function hasField($name)
+    {
+        return in_array($name, $this->getFieldList());
+    }
+
     /* Magic */
 
     public function __get($name)
     {
+        if ($this->hasField($name)) {
+            return $this->getField($name);
+        }
         if ($this->hasNamespace($name)) {
             return $this->getNamespace($name);
         }
@@ -40,7 +74,7 @@ abstract class UrlNamespace
 
     public function __isset($name)
     {
-        return $this->hasNamespace($name);
+        return $this->hasField($name) || $this->hasNamespace($name);
     }
 
     public function __set($name, $value)
